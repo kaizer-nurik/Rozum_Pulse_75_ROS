060becd7e833640028856bc8bdb282e6ae42f721
Kerimov Nuraddin <kerimovnm@mail.ru>
2025-08-11 10:37:20 +0300

    make working version

--- ros_solid/launch/display.py
+++ ros_solid/launch/display.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
 import os
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_path
+import xacro
 
 def generate_launch_description():
     # get package share directory
     pkg_ros_solid = get_package_share_directory('ros_solid')
 
     # full paths to your URDF and RVIZ config
-    urdf_file = os.path.join(pkg_ros_solid, 'urdf', 'ros_solid.urdf')
     rviz_config = os.path.join(pkg_ros_solid, 'urdf.rviz')
 
-    # read URDF so we can pass its contents on the 'robot_description' parameter
-    with open(urdf_file, 'r') as inf:
-        robot_description_content = inf.read()
 
+    xacro_file = get_package_share_path('ros_solid') / 'urdf' / 'ros_solid.urdf'
+    robot_description_config = xacro.process_file(str(xacro_file)).toxml()
     return LaunchDescription([
-
         # joint_state_publisher_gui
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
             parameters=[{
-                'robot_description': robot_description_content
-            }]
+                'robot_description': robot_description_config
+            }],
+            
         ),
 
         # robot_state_publisher
@@ -34,8 +33,9 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
+            output='screen',
             parameters=[{
-                'robot_description': robot_description_content
+                'robot_description': robot_description_config
             }]
         ),
 
